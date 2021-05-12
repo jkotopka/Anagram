2cd3972d57756ac46dbb6baf52f73432921ee447
John Kotopka <jkotopka@gmail.com>
2021-05-12 10:05:53 -0500

    Implement options to include/exclude words, min/max word length, etc.

--- src/org/kotopka/Anagram.java
+++ src/org/kotopka/Anagram.java
@@ -12,13 +12,51 @@ public class Anagram {
 
     private final Dictionary dictionary;
     private int count;
-    private int max;
     private int minWordLength;
+    private int maxWordLength;
+    private int maxResults;
+
+    private String includeWord;
+
+    private boolean excludeDuplicates;
+    private final Set<String> excludeWordsSet;
 
     public Anagram(String dictFile) {
         Objects.requireNonNull(dictFile, "Constructor argument cannot be null");
 
         this.dictionary = new Dictionary(dictFile);
+        this.maxWordLength = Integer.MAX_VALUE;
+        this.maxResults = Integer.MAX_VALUE;
+        this.includeWord = "";
+        this.excludeWordsSet = new HashSet<>();
+    }
+
+    private void setMinWordLength(int length) {
+        if (length <= 0) throw new IllegalArgumentException("Length must be positive");
+
+        minWordLength = length;
+    }
+
+    public void setMaxWordLength(int length) {
+        if (length <= 0) throw new IllegalArgumentException("Length must be positive");
+
+        maxWordLength = length;
+    }
+
+    public void includeWord(String word) {
+        Objects.requireNonNull(word, "Word cannot be null");
+
+        includeWord = word;
+    }
+
+    public void excludeWord(String word) {
+        Objects.requireNonNull(word, "Word cannot be null");
+
+        excludeWordsSet.add(word);
+    }
+
+    public void shouldExcludeDuplicates(boolean shouldExclude) {
+        excludeDuplicates = shouldExclude;
     }
 
     public List<String> findSingleAnagramsOf(String word) {
@@ -29,37 +67,40 @@ public class Anagram {
         return dictionary.getListOrEmpty(sortedWord);
     }
 
-    public List<String> findAllAnagramsOf(String word, int minWordLength, int max) {
+    public List<String> findAllAnagramsOf(String word) {
         LinkedList<String> stack = new LinkedList<>();
         List<String> anagrams    = new ArrayList<>();
 
-        resetInstanceFieldValues(minWordLength, max);
+        count = 0;
+
         buildAnagramList(word, stack, anagrams);
         Collections.sort(anagrams);
 
         return anagrams;
     }
 
-    private void resetInstanceFieldValues(int minWordLength, int max) {
-        this.minWordLength = minWordLength;
-        this.max   = max;
-        this.count = 0;
-    }
-
     private void buildAnagramList(String word, LinkedList<String> stack, List<String> anagrams) {
         for (String s : findAllValidSubWordsAsSet(word)) {
+            if (excludeWordsSet.contains(s)) continue;
+            if (excludeDuplicates) excludeWordsSet.add(s);
+
             stack.push(s);
 
-            String diff = Word.subtract(Word.sortLetters(word), Word.sortLetters(s));
+            String diff = Word.subtract(word, s);
 
             if (diff.isBlank()) {
-                anagrams.add(String.join(" ", stack));
-                count++;
+                if (includeWord.isBlank() || stack.contains(includeWord)) {
+                    anagrams.add(String.join(" ", stack));
+                    count++;
+                }
             }
 
-            if (count == max) return;
+            if (count == maxResults) return;
 
             buildAnagramList(diff, stack, anagrams);
+
+            if (excludeDuplicates) excludeWordsSet.remove(s);
+
             stack.pop();
         }
     }
@@ -69,9 +110,13 @@ public class Anagram {
 
         Set<String> validSubWords = new TreeSet<>();
 
-        for (String subString : generateSubStrings(word))
-            if(subString.length() >= minWordLength)
+        for (String subString : generateSubStrings(word)) {
+            int len = subString.length();
+
+            if(len >= minWordLength && len <= maxWordLength && !excludeWordsSet.contains(word))
                 validSubWords.addAll(dictionary.getListOrEmpty(subString));
+        }
+
 
         return validSubWords;
     }
@@ -99,7 +144,23 @@ public class Anagram {
             System.exit(0);
         }
 
-        Anagram anagram = new Anagram("corncob-lowercase.txt");
+        Anagram anagram = new Anagram("dictionary-large.txt");
+
+
+
+        // for "base ball"
+        anagram.excludeWord("bas");
+        anagram.excludeWord("lb");
+        anagram.excludeWord("bs");
+        anagram.excludeWord("ab");
+//        anagram.includeWord("ball");
+        anagram.setMinWordLength(2);
+        anagram.setMaxWordLength(4);
+        anagram.shouldExcludeDuplicates(true);
+
+
+
+
         String word = String.join(" ",args);
 
         Set<String> allSubWords = anagram.findAllValidSubWordsAsSet(word);
@@ -111,7 +172,7 @@ public class Anagram {
             allSubWords.forEach(System.out::println);
         }
 
-        List<String> allAnagrams = anagram.findAllAnagramsOf(word, 3, 1000);
+        List<String> allAnagrams = anagram.findAllAnagramsOf(word);
 
         if (allAnagrams.isEmpty()) {
             System.out.printf("\nNo anagrams of \"%s\" found\n", word);
