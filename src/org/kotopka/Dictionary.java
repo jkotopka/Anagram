644249ea009f54f82a15b095eac3a37fbffb9691
John Kotopka <jkotopka@gmail.com>
2021-05-12 14:02:55 -0500

    refactor Dictionary to use a Builder

--- src/org/kotopka/Dictionary.java
+++ src/org/kotopka/Dictionary.java
@@ -2,20 +2,92 @@ package org.kotopka;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.util.*;
 import java.util.stream.Stream;
 
 public class Dictionary {
 
+    public static class Builder {
+        private int minWordLength;
+        private int maxWordLength;
+        private Path dictFile;
+        private final Set<String> excludeWordsSet;
+
+        public Builder(Path dictFile) {
+            Objects.requireNonNull(dictFile);
+
+            this.minWordLength = 1;
+            this.maxWordLength = Integer.MAX_VALUE;
+            this.dictFile = dictFile;
+            this.excludeWordsSet = new HashSet<>();
+        }
+
+        public Builder setMinWordLength(int minWordLength) {
+            if (minWordLength < 0)
+                throw new IllegalArgumentException("Invalid min word length: " + minWordLength);
+
+            this.minWordLength = minWordLength;
+
+            return this;
+        }
+
+        public Builder setMaxWordLength(int maxWordLength) {
+            if (maxWordLength < 0)
+                throw new IllegalArgumentException("Invalid max word length: " + minWordLength);
+
+            this.maxWordLength = maxWordLength;
+
+            return this;
+        }
+
+        public Builder setDictionaryFile(Path filename) {
+            Objects.requireNonNull(filename);
+
+            this.dictFile = filename;
+
+            return this;
+        }
+
+        public Builder excludeWord(String word) {
+            // XXX: might not really matter if null is inserted into a HashSet
+            Objects.requireNonNull(word);
+
+            excludeWordsSet.add(word.toLowerCase());
+
+            return this;
+        }
+
+        public Builder excludeWordsFromSet(Set<String> words) {
+            Objects.requireNonNull(words);
+
+            for (String w : words)
+                if (w != null) excludeWordsSet.add(w.toLowerCase());
+
+            return this;
+        }
+
+        public Dictionary build() {
+            if (maxWordLength < minWordLength)
+                throw new IllegalArgumentException("Max word length cannot be smaller than min word length");
+            else
+                return new Dictionary(this);
+        }
+
+    }
+
+    private final int minWordLength;
+    private final int maxWordLength;
+    private final Set<String> excludeWordsSet;
     private final Map<String, List<String>> dictionary;
 
-    public Dictionary(String dictFile) {
-        Objects.requireNonNull(dictFile, "Constructor argument cannot be null");
-
+    private Dictionary(Builder builder) {
+        this.minWordLength = builder.minWordLength;
+        this.maxWordLength = builder.maxWordLength;
+        this.excludeWordsSet = builder.excludeWordsSet;
         this.dictionary = new HashMap<>();
 
-        try (Stream<String> dfs = Files.lines(Paths.get(dictFile))) {
+        try (Stream<String> dfs = Files.lines(builder.dictFile)) {
             dfs.forEach(this::addWordToDictionary);
         } catch (IOException e) {
             System.err.println("Error building dictionary file!");
@@ -29,7 +101,10 @@ public class Dictionary {
         if (!dictionary.containsKey(sortedWord))
             dictionary.put(sortedWord, new ArrayList<>());
 
-        dictionary.get(sortedWord).add(word);
+        int wordLen = sortedWord.length();
+
+        if (wordLen >= minWordLength && wordLen <= maxWordLength && !excludeWordsSet.contains(word))
+            dictionary.get(sortedWord).add(word);
     }
 
     public boolean containsWord(String word) {
