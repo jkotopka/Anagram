056142b51a17ba75f33f067a07d99f83ae8bc81d
John Kotopka <jkotopka@gmail.com>
2021-05-12 14:36:04 -0500

    refactor Anagram, add Builder, other significant refactoring
    Tons of stuff in this commit, probably too much.
    
    First, the class now uses a Builder to set the initial state before building a new Anagram object
    
    Second, lots of private setter methods are commented-out because the same functionality is now provided by the builder. However, might want to reimplement these methods later.
    
    Third, findAllValidSubWordsAsSet now returns a TreeSet<String> so that the resulting set can invoke the TreeSet.tailSet(item) method to get a subset starting from "item". The plan is to be able to generate anagrams from a starting point.
    
    Fourth, there were other smaller refactorings, like extracting the validateAndAddAnagramToList() method, small tweaks, changes to main(), and probably more

--- src/org/kotopka/Anagram.java
+++ src/org/kotopka/Anagram.java
@@ -1,5 +1,6 @@
 package org.kotopka;
 
+import java.nio.file.Paths;
 import java.util.*;
 
 /**
@@ -10,60 +11,141 @@ import java.util.*;
  */
 public class Anagram {
 
+    public static class Builder {
+        private final String dictFile;
+        private int minWordLength;
+        private int maxWordLength;
+        private int maxResults;
+
+        private String includeWord;
+
+        private boolean excludeDuplicates;
+        private Set<String> excludeWordsSet;
+
+        public Builder(String dictFile) {
+            Objects.requireNonNull(dictFile);
+
+            this.dictFile = dictFile;
+            this.minWordLength = 1;
+            this.maxWordLength = Integer.MAX_VALUE;
+            this.maxResults = Integer.MAX_VALUE;
+            this.includeWord = "";
+            this.excludeWordsSet = new HashSet<>();
+        }
+
+        public Builder setMinWordLength(int minWordLength) {
+            if (minWordLength < 0)
+                throw new IllegalArgumentException("Invalid min word length: " + minWordLength);
+
+            this.minWordLength = minWordLength;
+
+            return this;
+        }
+
+        public Builder setMaxWordLength(int maxWordLength) {
+            if (maxWordLength < 0)
+                throw new IllegalArgumentException("Invalid max word length: " + maxWordLength);
+
+            this.maxWordLength = maxWordLength;
+
+            return this;
+        }
+
+        public Builder setMaxResults(int maxResults) {
+            if (maxResults < 0)
+                throw new IllegalArgumentException("Invalid max results: " + maxResults);
+
+            this.maxResults = maxResults;
+
+            return this;
+        }
+
+        public Builder includeWord(String word) {
+            Objects.requireNonNull(word);
+
+            this.includeWord = word;
+
+            return this;
+        }
+
+        public Builder excludeDuplicates(boolean shouldExclude) {
+            this.excludeDuplicates = shouldExclude;
+
+            return this;
+        }
+
+        public Builder excludeWordSet(Set<String> excludeWordsSet) {
+            Objects.requireNonNull(excludeWordsSet);
+
+            // do NOT want an immutable copy here because it gets updated during use
+            this.excludeWordsSet = excludeWordsSet;
+
+            return this;
+        }
+
+        public Anagram build() {
+            return new Anagram(this);
+        }
+
+    }
+
     private final Dictionary dictionary;
     private int count;
     private int minWordLength;
     private int maxWordLength;
     private int maxResults;
-
     private String includeWord;
-
     private boolean excludeDuplicates;
     private final Set<String> excludeWordsSet;
 
-    public Anagram(String dictFile) {
-        Objects.requireNonNull(dictFile, "Constructor argument cannot be null");
+    private Anagram(Builder builder) {
+        this.minWordLength = builder.minWordLength;
+        this.maxWordLength = builder.maxWordLength;
+        this.maxResults = builder.maxResults;
+        this.includeWord = builder.includeWord;
+        this.excludeDuplicates = builder.excludeDuplicates;
+        this.excludeWordsSet = builder.excludeWordsSet;
 
-        this.dictionary = new Dictionary(dictFile);
-        this.maxWordLength = Integer.MAX_VALUE;
-        this.maxResults = Integer.MAX_VALUE;
-        this.includeWord = "";
-        this.excludeWordsSet = new HashSet<>();
+        this.dictionary = new Dictionary.Builder(Paths.get(builder.dictFile))
+                .setMinWordLength(minWordLength)
+                .setMaxWordLength(maxWordLength)
+                .excludeWordsFromSet(excludeWordsSet)
+                .build();
     }
 
-    private void setMinWordLength(int length) {
-        if (length <= 0) throw new IllegalArgumentException("Length must be positive");
-
-        minWordLength = length;
-    }
-
-    public void setMaxWordLength(int length) {
-        if (length <= 0) throw new IllegalArgumentException("Length must be positive");
-
-        maxWordLength = length;
-    }
-
-    public void setMaxResults(int max) {
-        if (max <= 0) throw new IllegalArgumentException("Max must be positive");
-
-        maxResults = max;
-    }
-
-    public void includeWord(String word) {
-        Objects.requireNonNull(word, "Word cannot be null");
-
-        includeWord = word;
-    }
-
-    public void excludeWord(String word) {
-        Objects.requireNonNull(word, "Word cannot be null");
-
-        excludeWordsSet.add(word);
-    }
-
-    public void shouldExcludeDuplicates(boolean shouldExclude) {
-        excludeDuplicates = shouldExclude;
-    }
+//    private void setMinWordLength(int length) {
+//        if (length <= 0) throw new IllegalArgumentException("Length must be positive");
+//
+//        minWordLength = length;
+//    }
+//
+//    public void setMaxWordLength(int length) {
+//        if (length <= 0) throw new IllegalArgumentException("Length must be positive");
+//
+//        maxWordLength = length;
+//    }
+//
+//    public void setMaxResults(int max) {
+//        if (max <= 0) throw new IllegalArgumentException("Max must be positive");
+//
+//        maxResults = max;
+//    }
+//
+//    public void includeWord(String word) {
+//        Objects.requireNonNull(word, "Word cannot be null");
+//
+//        includeWord = word;
+//    }
+//
+//    public void excludeWord(String word) {
+//        Objects.requireNonNull(word, "Word cannot be null");
+//
+//        excludeWordsSet.add(word);
+//    }
+//
+//    public void shouldExcludeDuplicates(boolean shouldExclude) {
+//        excludeDuplicates = shouldExclude;
+//    }
 
     public List<String> findSingleAnagramsOf(String word) {
         Objects.requireNonNull(word, "Method argument cannot be null");
@@ -79,29 +161,26 @@ public class Anagram {
 
         count = 0;
 
-        buildAnagramList(word, stack, anagrams);
+        buildAnagramListRecursively(word, stack, anagrams);
         Collections.sort(anagrams);
 
         return anagrams;
     }
 
-    private void buildAnagramList(String word, LinkedList<String> stack, List<String> anagrams) {
+    private void buildAnagramListRecursively(String word, LinkedList<String> stack, List<String> anagrams) {
         for (String s : findAllValidSubWordsAsSet(word)) {
             if (excludeWordsSet.contains(s)) continue;
-            if (excludeDuplicates) excludeWordsSet.add(s);
+            if (excludeDuplicates) excludeWordsSet.add(s.toLowerCase());
 
             stack.push(s);
 
             String diff = Word.subtract(word, s);
 
-            if (diff.isBlank() && (includeWord.isBlank() || stack.contains(includeWord))) {
-                anagrams.add(String.join(" ", stack));
-                count++;
-            }
+            validateAndAddAnagramToList(diff, stack, anagrams);
 
             if (count == maxResults) return;
 
-            buildAnagramList(diff, stack, anagrams);
+            buildAnagramListRecursively(diff, stack, anagrams);
 
             if (excludeDuplicates) excludeWordsSet.remove(s);
 
@@ -109,16 +188,15 @@ public class Anagram {
         }
     }
 
-    public Set<String> findAllValidSubWordsAsSet(String word) {
+    // TODO: some kind of startAt ability to start the list at a certain word, TreeSet.tailSet()?
+    public TreeSet<String> findAllValidSubWordsAsSet(String word) {
         Objects.requireNonNull(word, "Method argument cannot be null");
 
-        Set<String> validSubWords = new TreeSet<>();
+        TreeSet<String> validSubWords = new TreeSet<>();
 
         for (String subString : generateSubStrings(word)) {
-            int len = subString.length();
 
-            if(len >= minWordLength && len <= maxWordLength && !excludeWordsSet.contains(word))
-                validSubWords.addAll(dictionary.getListOrEmpty(subString));
+            validSubWords.addAll(dictionary.getListOrEmpty(subString));
         }
 
 
@@ -142,33 +220,45 @@ public class Anagram {
         }
     }
 
+    private void validateAndAddAnagramToList(String diff, LinkedList<String> stack, List<String> anagrams) {
+        if (diff.isBlank() && (includeWord.isBlank() || stack.contains(includeWord))) {
+            anagrams.add(String.join(" ", stack));
+            count++;
+        }
+    }
+
+    // TODO: one of the words must have a certain suffix?
+    //  also includeWordSet and anagram validation
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Usage: java Anagram <word>");
             System.exit(0);
         }
 
-        Anagram anagram = new Anagram("dictionary-large.txt");
+        Set<String> toExclude = new HashSet<>();
+        toExclude.add(null);
+        toExclude.add("msc");
+        toExclude.add("acl");
+        toExclude.add("tko");
+        toExclude.add("acm");
+        toExclude.add("mph");
+        toExclude.add("acol");
+        toExclude.add("acs");
+        toExclude.add("HIMS");
 
-
-
-        // for "base ball"
-        anagram.excludeWord("bas");
-        anagram.excludeWord("lb");
-        anagram.excludeWord("bs");
-        anagram.excludeWord("ab");
-        anagram.includeWord("ball");
-        anagram.setMinWordLength(2);
-        anagram.setMaxWordLength(4);
-        anagram.setMaxResults(4);
-        anagram.shouldExcludeDuplicates(true);
-
-
-
+        Anagram anagram = new Anagram.Builder("dictionary-large.txt")
+                .setMinWordLength(2)
+                .setMaxWordLength(10)
+                .setMaxResults(1000)
+                .excludeDuplicates(false)
+                .excludeWordSet(toExclude)
+                .includeWord("labs")
+                .build();
 
         String word = String.join(" ",args);
 
-        Set<String> allSubWords = anagram.findAllValidSubWordsAsSet(word);
+        // TODO: check the tailSet() here later
+        Set<String> allSubWords = anagram.findAllValidSubWordsAsSet(word).tailSet("");
 
         if (allSubWords.isEmpty()) {
             System.out.printf("No sub-words of \"%s\" found\n", word);
