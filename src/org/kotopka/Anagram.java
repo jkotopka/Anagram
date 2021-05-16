6bce33d3014a93a7703bc8116e88fc9f7e7dc6bd
John Kotopka <jkotopka@gmail.com>
2021-05-15 19:25:28 -0500

    Remove superfluous TODO

--- src/org/kotopka/Anagram.java
+++ src/org/kotopka/Anagram.java
@@ -123,7 +123,6 @@ public final class Anagram {
 
     // TODO: refactored a bit with extracted methods, not sure if cleaner,
     //  mostly not sure if having the "recursive" call in another method is bad practice or not
-    // TODO: rename startWord startFromWord
     private void buildAnagramList(String word, String startWord, LinkedList<String> anagram, List<String> anagramList) {
         if (count == maxResults) return;
 
