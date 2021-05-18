81bb3c217ae18aee8c6859197e898b957b5b11f3
John Kotopka <jkotopka@gmail.com>
2021-05-18 10:19:56 -0500

    Retrieve objects from static factories

--- src/org/kotopka/NewTestClient.java
+++ src/org/kotopka/NewTestClient.java
@@ -1,6 +1,7 @@
 package org.kotopka;
 
-import org.kotopka.CommandlineParser.*;
+import org.kotopka.CommandlineParser.Parser;
+import org.kotopka.CommandlineParser.Switch;
 
 import java.util.List;
 import java.util.Set;
@@ -36,49 +37,13 @@ public class NewTestClient {
     }
 
     public static void main(String[] args) {
-        Parser parser = new CommandlineParser(args, Switch.COLLECT_PHRASE);
-        parser.addOptions(
-                new DictFile(),
-                new MinWordLen(),
-                new MaxWordLen(),
-                new ExcludeFile(),
-                new MaxResults(),
-                new Timeout(),
-                new MaxWords(),
-                new StartFrom(),
-                new IncludeWord(),
-                new ExcludeWord(),
-                new IncludeWordWithSuffix(),
-                new RestrictPermutations(),
-                new ExcludeDuplicates(),
-                new HelpMessage(),
-                new ExtendedHelpMessage(),
-                new CollectPhrase()
-        );
+        Parser commandlineParser = ParserFactory.getParser(args);
+        Dictionary dictionary = DictionaryFactory.getDictionary(commandlineParser);
+        Anagram anagram = AnagramFactory.getAnagram(commandlineParser, dictionary);
 
-        parser.parseArgs();
+        commandlineParser.printState();
 
-        Dictionary dictionary = new Dictionary.Builder("dictionary-large.txt")
-                .setDictionaryFile(parser.getOption(Switch.DICT_FILE).getString())
-                .setMaxWordLength(parser.getOption(Switch.MAX_WORD_LENGTH).getInt())
-                .setMinWordLength(parser.getOption(Switch.MIN_WORD_LENGTH).getInt())
-                .excludeWordsFromFile(parser.getOption(Switch.EXCLUDE_FROM_DICT_FILE).getString())
-                .build();
-
-        Anagram anagram = new Anagram(dictionary)
-                .setMaxResults(parser.getOption(Switch.MAX_RESULTS).getInt())
-                .setMaxWordsInAnagram(parser.getOption(Switch.MAX_WORDS).getInt())
-                .setMaxTimeoutInSeconds(parser.getOption(Switch.TIMEOUT).getInt())
-                .shouldExcludeDuplicates(parser.getOption(Switch.EXCLUDE_DUPLICATES).getBool())
-                .shouldRestrictPermutations(parser.getOption(Switch.RESTRICT_PERMUTATIONS).getBool())
-                .startFrom(parser.getOption(Switch.START_FROM).getString())
-                .includeWord(parser.getOption(Switch.INCLUDE_WORD).getString())
-                .excludeWord(parser.getOption(Switch.EXCLUDE_WORD).getString())
-                .includeWordWithSuffix(parser.getOption(Switch.INCLUDE_WORD_WITH_SUFFIX).getString());
-
-        parser.printState();
-
-        String word = parser.getOption(Switch.COLLECT_PHRASE).getString();
+        String word = commandlineParser.getOption(Switch.COLLECT_PHRASE).getString();
 
         findAndPrintSubWords(anagram, word);
         findAndPrintAnagrams(anagram, word);
