f36e9787fe5e770aa25941d2ee4cd331dbd24735
John Kotopka <jkotopka@gmail.com>
2021-05-18 19:03:40 -0500

    Add class explanation comment

--- src/org/kotopka/parser/PrintState.java
+++ src/org/kotopka/parser/PrintState.java
@@ -2,6 +2,11 @@ package org.kotopka.parser;
 
 public class PrintState extends AbstractBooleanOption {
 
+    // XXX: this class doesn't work as expected. rather than print the state it only sets the boolean flag
+    // to inform the client whether the option was selected. it is the client's responsibility to actually
+    // print the state information. the reason for this is because the parser is only able to get the full
+    // commandline state after all input has been entered and attempting to print the state from here would
+    // be premature and not reflective of all commandline input
     public PrintState() {
         this.commandlineSwitch = Switch.PRINT_STATE;
         this.description = "Print state";
