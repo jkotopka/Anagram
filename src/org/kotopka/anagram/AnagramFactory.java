c2c2bb3598d2374eabbafcbc312647aee3f7b57b
John Kotopka <jkotopka@gmail.com>
2021-05-19 18:45:39 -0500

    Rename "set" methods

--- src/org/kotopka/anagram/AnagramFactory.java
+++ src/org/kotopka/anagram/AnagramFactory.java
@@ -11,11 +11,11 @@ public class AnagramFactory {
                 .setMaxResults(parser.getOption(Switch.MAX_RESULTS).getInt())
                 .setMaxWordsInAnagram(parser.getOption(Switch.MAX_WORDS).getInt())
                 .setMaxTimeoutInSeconds(parser.getOption(Switch.TIMEOUT).getInt())
-                .shouldExcludeDuplicates(parser.getOption(Switch.EXCLUDE_DUPLICATES).getBool())
-                .shouldRestrictPermutations(parser.getOption(Switch.RESTRICT_PERMUTATIONS).getBool())
-                .startFrom(parser.getOption(Switch.START_FROM).getString())
-                .includeWord(parser.getOption(Switch.INCLUDE_WORD).getString())
-                .excludeWord(parser.getOption(Switch.EXCLUDE_WORD).getString())
-                .includeWordWithSuffix(parser.getOption(Switch.INCLUDE_WORD_WITH_SUFFIX).getString());
+                .setShouldExcludeDuplicates(parser.getOption(Switch.EXCLUDE_DUPLICATES).getBool())
+                .setShouldRestrictPermutations(parser.getOption(Switch.RESTRICT_PERMUTATIONS).getBool())
+                .setStartFrom(parser.getOption(Switch.START_FROM).getString())
+                .setIncludeWord(parser.getOption(Switch.INCLUDE_WORD).getString())
+                .setExcludeWord(parser.getOption(Switch.EXCLUDE_WORD).getString())
+                .setIncludeWordWithSuffix(parser.getOption(Switch.INCLUDE_WORD_WITH_SUFFIX).getString());
     }
 }
