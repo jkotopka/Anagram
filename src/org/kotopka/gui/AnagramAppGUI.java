d9a2bf45541ccc955adca605fca2fa3b6f7ff508
John Kotopka <jkotopka@gmail.com>
2021-05-22 15:15:43 -0500

    Add AnagramAppGUI.java

---
+++ src/org/kotopka/gui/AnagramAppGUI.java
@@ -0,0 +1,10 @@
+package org.kotopka.gui;
+
+import org.kotopka.gui.controller.MainController;
+
+public class AnagramAppGUI {
+
+    public static void main(String[] args) {
+        MainController controller = new MainController();
+    }
+}
