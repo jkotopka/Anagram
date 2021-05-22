946006223b2b104f72a12f3f4b6d4a18916fa2cc
John Kotopka <jkotopka@gmail.com>
2021-05-22 15:36:36 -0500

    Implement basic functionality

--- src/org/kotopka/gui/controller/MainController.java
+++ src/org/kotopka/gui/controller/MainController.java
@@ -1,5 +1,7 @@
 package org.kotopka.gui.controller;
 
+import org.kotopka.anagram.Anagram;
+import org.kotopka.dictionary.Dictionary;
 import org.kotopka.gui.view.MainFrame;
 
 public class MainController {
@@ -8,5 +10,17 @@ public class MainController {
 
     public MainController() {
         this.frame = new MainFrame();
+
+        Dictionary dictionary = new Dictionary.Builder("dictionary-large.txt").build();
+        Anagram anagram = new Anagram(dictionary);
+
+        frame.addActionListener(e -> {
+            String anagramString = frame.getAnagramString();
+            java.util.List<String> anagrams = anagram.findMultipleWordAnagramsOf(anagramString);
+            java.util.Set<String> subWords = anagram.findAllValidSubWordsAsSet(anagramString);
+
+            frame.setAnagram(anagrams);
+            frame.setSubWords(subWords);
+        });
     }
 }
