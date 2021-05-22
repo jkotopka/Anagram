11bec8d3ac95c17330ac88cb59da2cbccf2fc068
John Kotopka <jkotopka@gmail.com>
2021-05-22 15:15:51 -0500

    Add MainController.java

---
+++ src/org/kotopka/gui/controller/MainController.java
@@ -0,0 +1,12 @@
+package org.kotopka.gui.controller;
+
+import org.kotopka.gui.view.MainFrame;
+
+public class MainController {
+
+    private final MainFrame frame;
+
+    public MainController() {
+        this.frame = new MainFrame();
+    }
+}
