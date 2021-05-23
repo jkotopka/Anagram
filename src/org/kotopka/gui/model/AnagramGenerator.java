fc747e310b9fdb2c548980445b4b3c81f7032cdb
John Kotopka <jkotopka@gmail.com>
2021-05-23 11:31:41 -0500

    Use Parser to build Dictionary and Anagram objects

--- src/org/kotopka/gui/model/AnagramGenerator.java
+++ src/org/kotopka/gui/model/AnagramGenerator.java
@@ -1,16 +1,18 @@
 package org.kotopka.gui.model;
 
 import org.kotopka.anagram.Anagram;
+import org.kotopka.anagram.AnagramFactory;
 import org.kotopka.dictionary.Dictionary;
+import org.kotopka.dictionary.DictionaryFactory;
+import org.kotopka.parser.Parser;
 
 public class AnagramGenerator {
 
-    private final Dictionary dictionary;
     private final Anagram anagram;
 
-    public AnagramGenerator() {
-        this.dictionary = new Dictionary.Builder("dictionary-large.txt").build();
-        this.anagram = new Anagram(dictionary);
+    public AnagramGenerator(Parser parser) {
+        Dictionary dictionary = DictionaryFactory.getDictionary(parser);
+        this.anagram = AnagramFactory.getAnagram(parser, dictionary);
     }
 
     public Anagram getAnagram() {
