c1e9d782a92c008daff53678b49775c9134295d3
John Kotopka <jkotopka@gmail.com>
2021-05-23 10:44:53 -0500

    Add method

--- src/org/kotopka/gui/view/OptionsDialog.java
+++ src/org/kotopka/gui/view/OptionsDialog.java
@@ -4,10 +4,22 @@ import javax.swing.*;
 
 public class OptionsDialog extends JDialog {
 
+    private final OptionsPanel optionsPanel;
+
     public OptionsDialog(MainFrame frame) {
+
+        this.optionsPanel = new OptionsPanel(this);
+
         setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        setContentPane(optionsPanel);
         setModal(true);
-        setSize(400, 400);
+//        setSize(400, 400);
+        pack();
         setLocationRelativeTo(frame);
     }
+
+    public String[] getOptionsArgs() {
+        return optionsPanel.getOptionsArgs();
+    }
+
 }
