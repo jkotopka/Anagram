925dc79f9b785aead372daba53aa9f3ef8471576
John Kotopka <jkotopka@gmail.com>
2021-05-23 11:31:16 -0500

    Implement anagram generation with options

--- src/org/kotopka/gui/controller/MainController.java
+++ src/org/kotopka/gui/controller/MainController.java
@@ -3,21 +3,30 @@ package org.kotopka.gui.controller;
 import org.kotopka.anagram.Anagram;
 import org.kotopka.gui.model.AnagramGenerator;
 import org.kotopka.gui.view.MainFrame;
+import org.kotopka.parser.Parser;
+import org.kotopka.parser.ParserFactory;
+
+import java.util.Arrays;
 
 public class MainController {
 
     private final MainFrame frame;
-    private final AnagramGenerator anagramGenerator;
 
     public MainController() {
         this.frame = new MainFrame();
-        this.anagramGenerator = new AnagramGenerator();
 
-        invokeAnagramGenerate(anagramGenerator.getAnagram());
+        invokeAnagramGenerate();
     }
 
-    private void invokeAnagramGenerate(Anagram anagram) {
+    private void invokeAnagramGenerate() {
         frame.addActionListener(e -> {
+            String[] args = frame.getOptions();
+
+            System.out.println("In invokeAnagramGenerate(): " + Arrays.toString(args));
+
+            Parser parser = ParserFactory.getParser(args);
+            AnagramGenerator anagramGenerator = new AnagramGenerator(parser);
+            Anagram anagram = anagramGenerator.getAnagram();
             String anagramString = frame.getAnagramString();
             java.util.List<String> anagrams = anagram.findMultipleWordAnagramsOf(anagramString);
             java.util.Set<String> subWords = anagram.findAllValidSubWordsAsSet(anagramString);
